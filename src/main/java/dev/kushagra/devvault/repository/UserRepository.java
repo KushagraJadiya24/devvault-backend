e64d2a7c2c7bb47a2a7cacbb5c433ab2dfd0b3b6
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-06-30 23:47:29 +0530

    chore: User Repository added

---
+++ src/main/java/dev/kushagra/devvault/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package dev.kushagra.devvault.repository;
+
+import dev.kushagra.devvault.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User,Long> {
+
+    Optional<User> findByEmail(String email);
+
+}
