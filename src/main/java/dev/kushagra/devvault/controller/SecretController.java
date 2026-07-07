9559a14644acb0ef6e15c10658617beee371dc35
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-07-08 00:11:56 +0530

    chore: Secret Controller PostMapping test

--- src/main/java/dev/kushagra/devvault/controller/SecretController.java
+++ src/main/java/dev/kushagra/devvault/controller/SecretController.java
@@ -1,4 +1,26 @@
 package dev.kushagra.devvault.controller;
 
+import dev.kushagra.devvault.dto.SecretRequest;
+import dev.kushagra.devvault.service.SecretService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/secrets")
+@RequiredArgsConstructor
 public class SecretController {
+    private final SecretService secretService;
+
+    @PostMapping("/api/secrets")
+    @PreAuthorize("hasRole('ADMIN')")
+    public String createSecret(@Valid @RequestBody SecretRequest request){
+        Long userId = (Long) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+
+        String Secret = secretService.createSecret(request,userId);
+
+        return Secret;
+    }
 }
