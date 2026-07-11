f229e6014542be0b37dd67a8224c7dcef2f23d8b
Kushagra Jadiya <kjadiya2004@gmail.com>
2026-07-11 23:11:16 +0530

    chore: Adding CRUD Requests to Controller

--- src/main/java/dev/kushagra/devvault/controller/SecretController.java
+++ src/main/java/dev/kushagra/devvault/controller/SecretController.java
@@ -5,6 +5,8 @@ import dev.kushagra.devvault.model.Secret;
 import dev.kushagra.devvault.service.SecretService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -28,5 +30,30 @@ public class SecretController {
     @PreAuthorize("hasRole('ADMIN') or hasRole('MEMBER')")
     public String getSecret(@PathVariable String name) throws Exception {
         return secretService.getSecretByName(name);
+    }
+
+    @GetMapping
+    @PreAuthorize("hasRole('ADMIN') or hasRole('MEMBER')")
+    public Page<Secret> getAllSecrets(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size) {
+        return secretService.getAllSecrets(page, size);
+    }
+
+    @PutMapping("/{name}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public Secret updateSecret(
+            @PathVariable String name,
+            @RequestBody String newValue) throws Exception {
+        Long userId = (Long) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        return secretService.updateSecret(name, newValue, userId);
+    }
+
+    @DeleteMapping("/{name}")
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Void> deleteSecret(@PathVariable String name) {
+        secretService.deleteSecret(name);
+        return ResponseEntity.noContent().build();
     }
 }
